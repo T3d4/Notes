8c21e011659f974e9de0ddb7d2017baf2ea0c949
tedabytez <tdansa64@gmail.com>
2023-08-16 15:31:43 +0100

    added deleteBtn and logic

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,107 @@
-<script setup>
-import { ref } from 'vue';
-const showModal = ref(false);
-const newNote = ref('');
-const notes = ref([]);
-const errorMessage = ref('');
+<script async setup>
+import { ref } from "vue";
+import config from "./config";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  onSnapshot,
+  deleteDoc,
+  collection,
+  addDoc,
+  doc,
+  query,
+} from "firebase/firestore";
 
-function getRandomColor(){
+const app = initializeApp(config);
+const db = getFirestore(app);
+const showModal = ref(false);
+const newNote = ref("");
+const notes = ref([]);
+const errorMessage = ref("");
+
+function getRandomColor() {
   return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
 }
 
-const addNote = () => {
-  if(newNote.value.length < 9){
-    return errorMessage.value = 'Note needs 10 characters or more';
+const fDate = (date) => {
+  const dateInput = new Date(date);
+  return dateInput.toLocaleDateString("en-UK");
+};
+
+const q = query(collection(db, "notes"));
+const dbNotes = onSnapshot(q, (note) => {
+  console.log(note);
+
+  note.docChanges().forEach((n) => {
+    const existingNote = notes.value.find((note) => note.id === n.doc.id);
+    if (!existingNote) {
+      if (n.type === "added") {
+        notes.value.push({
+          id: n.doc.id,
+          text: n.doc.data().text,
+          date: fDate(n.doc.data().date.toDate()),
+          bgColor: n.doc.data().bgColor,
+        });
+      }
+    }
+  });
+});
+
+const addNote = async () => {
+  if (newNote.value.length < 9) {
+    return (errorMessage.value = "Note needs 10 characters or more");
   }
-  notes.value.push({
-    id: Math.floor(Math.random()*1000000),
+  addDoc(collection(db, "notes"), {
     text: newNote.value,
     date: new Date(),
-    backgroundColor: getRandomColor()
+    bgColor: getRandomColor(),
   });
   showModal.value = false;
-  newNote.value = '';
-  errorMessage.value = '';
-}
+  newNote.value = "";
+  errorMessage.value = "";
+  await Promise.all([addNote()]);
+};
+
+const deleteNote = async (noteId) => {
+  await deleteDoc(doc(db, "notes", noteId));
+};
 </script>
 
 <template>
   <main>
     <div v-if="showModal" class="overlay">
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10" v-model.trim="newNote"></textarea>
-        <p v-if="errorMessage"> {{ errorMessage }} </p>
+        <textarea
+          name="note"
+          id="note"
+          cols="30"
+          rows="10"
+          v-model.trim="newNote"
+        ></textarea>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
         <button @click="addNote">Add note</button>
-        <button @click="showModal=false" class="close">close</button>
+        <button @click="showModal = false" class="close">close</button>
       </div>
     </div>
     <div class="container">
       <header>
         <h1>Notes</h1>
-        <button @click="showModal=true">+</button>
+        <button @click="showModal = true">+</button>
       </header>
       <div class="cards-container">
-        <div class="card" 
-              v-for="note in notes"
-              :key="note.id" 
-              :style="{backgroundColor: note.backgroundColor}">
-          <p class="main-text"> {{ note.text }} </p>
-          <p class="date"> {{ note.date.toLocaleDateString('en-UK') }} </p>
+        <div
+          class="card"
+          v-for="note in notes"
+          :key="note.id"
+          :style="{ backgroundColor: note.bgColor }"
+        >
+          <div class="delete-btn">
+            <button @click="deleteNote(note.id)" type="button">
+              <font-awesome-icon icon="fa-solid fa-xmark" size="lg" />
+            </button>
+          </div>
+          <p class="main-text">{{ note.text }}</p>
+          <p class="date">{{ note.date }}</p>
         </div>
       </div>
     </div>
@@ -54,107 +109,125 @@ const addNote = () => {
 </template>
 
 <style scoped>
-  main{
-    height: 100vh;
-    width: 100vw;
-  }
+main {
+  height: 100vh;
+  width: 100vw;
+}
 
-  .container {
-    max-width: 800px;
-    padding: 10px;
-    margin: 0 auto;
-  }
+.container {
+  max-width: 80%;
+  padding: 10px;
+  margin: 0 auto;
+}
 
-  header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  h1{
-    font-weight: bold;
-    margin-bottom: 25px;
-    font-size: 75px;
-  }
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
 
-  header
-  button{
-    border: none;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    background-color: steelblue;
-    border-radius: 100%;
-    color: white;
-    font-size: 20px;
-  }
+header button {
+  border: none;
+  padding: 10px;
+  width: 70px;
+  height: 70px;
+  cursor: pointer;
+  background-color: steelblue;
+  border-radius: 100%;
+  color: white;
+  font-size: 40px;
+  margin-right: 30%;
+}
 
-  .card{
-    width: 225px;
-    height: 225px;
-    background-color: rgb(237, 182, 44);
-    padding: 10px;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin-right: 20px;
-    margin-bottom: 20px;
-    color: black;
-  }
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  color: black;
+  word-wrap: break-word;
+  box-sizing: border-box;
+  transition: transform 0.6s;
+  transition-property: font-size;
+}
 
-  .date{
-    font-size: 12.5px;
-    font-weight: bold;
-  }
+.card:hover {
+  transform: scale(1.05);
+  font-size: large;
+}
 
-  .cards-container{
-    display: flex;
-    flex-wrap: wrap;
-  }
+.date {
+  font-size: 12.5px;
+  font-weight: bold;
+  margin-top: auto;
+}
 
-  .overlay{
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.7);
-    align-items: center;
-    justify-content: center;
-    display: flex;
-    z-index: 10;
-  }
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
 
-  .modal
-  button{
-    padding: 10px 20px;
-    font-size: 20px;
-    width: 100%;
-    background-color: blueviolet;
-    border: none;
-    color: white;
-    margin-top: 15px;
-    cursor: pointer;
-  }
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  z-index: 10;
+}
 
-  .modal{
-    width: 700px;
-    background-color: white;
-    border-radius: 10px;
-    padding: 30px;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    margin: 0 auto;
-  }
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  margin-top: 15px;
+  cursor: pointer;
+}
 
-  .modal .close{
-    background-color: rgb(146, 9, 9);
-    margin-top: 7px;
-  }
+.modal {
+  width: 700px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
 
-  .modal
-  p{
-    color: rgb(170, 9, 9);
-  }
+.modal .close {
+  background-color: rgb(146, 9, 9);
+  margin-top: 7px;
+}
+
+.modal p {
+  color: rgb(170, 9, 9);
+}
+
+.delete-btn button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  float: right;
+  transition: transform 0.2ms;
+}
+
+.delete-btn button:hover {
+  transform: scale(1.6);
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faXmark)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
